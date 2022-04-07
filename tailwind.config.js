81ddd136fccb03048e677a38bc446c5e6edb0e6f
yusuf <yusuffront@gmail.com>
2022-04-07 16:23:32 +0800

    new project portofolio --

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors : {
+        primary: '#a855f7',
+        subtitle: '#3f3f46'
+      },
+      screens: {
+        '2xl': '1034px',
+      },
+      fontFamily: {
+        'lobs': 'Lobster, cursive'
+      }
+    },
+  },
+  plugins: [],
+}
