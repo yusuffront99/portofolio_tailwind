8014c39f00393691b555979c32901cb4118b3126
yusuf <yusuffront@gmail.com>
2022-04-13 12:19:21 +0800

    updated more codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -9,7 +10,8 @@ module.exports = {
       colors : {
         primary: '#a855f7',
         subtitle: '#3f3f46',
-        secondary: '#64748b'
+        secondary: '#64748b',
+        darkmode: '#1e293b'
       },
       screens: {
         '2xl': '1034px',
